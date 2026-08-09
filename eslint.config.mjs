7539df9542160fee886337d03f0bc0b87fd12612
Brandon <brandon.tromp@gmail.com>
2026-08-09 10:03:05 +0200

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,11 @@ const eslintConfig = defineConfig([
     'next-env.d.ts',
   ]),
   ...storybook.configs['flat/recommended'],
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  },
 ])
 
 export default eslintConfig
